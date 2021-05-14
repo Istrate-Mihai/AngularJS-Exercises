934603a3955f6e939e516e7fcec7e12dbd9be8d4
Istrate Mihai <i.mihai9960@gmail.com>
2021-05-14 14:52:03 +0300

    Started a new folder with AJAX applications

---
+++ Angular-AJAX-Exercises/php_Files/persons.php
@@ -0,0 +1,14 @@
+<?php
+
+
+echo '[
+
+{"name":"Person1","occupation":"Occupation1"},
+{"name":"Person2","occupation":"Occupation2"},
+{"name":"Person3","occupation":"Occupation3"},
+{"name":"Person4","occupation":"Occupation4"}
+
+
+]';
+
+?>
